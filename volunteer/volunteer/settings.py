156b2caaddb680fa4f53fc7e08b403a6a0538c0d
Ruslan <ruslanpercovic391@gmail.com>
2026-02-23 22:36:57 +0200

    diploma

--- volunteer/volunteer/settings.py
+++ volunteer/volunteer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'volunteer_app.apps.VolunteerAppConfig',
+    'volunteer_app',
 ]
 
 MIDDLEWARE = [
