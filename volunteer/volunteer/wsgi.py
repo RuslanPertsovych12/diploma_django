49e96b57a82058871b4ba21f4f2cd0586ae81e5a
Ruslan <ruslanpercovic391@gmail.com>
2026-02-23 22:24:52 +0200

    diploma

--- volunteer/volunteer/wsgi.py
+++ volunteer/volunteer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer.volunteer.settings')
 
 application = get_wsgi_application()
