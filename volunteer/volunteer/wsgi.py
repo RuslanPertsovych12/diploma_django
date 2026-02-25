b5922d5fac883ac39ec5024fae2be0b6b65c1f1c
Ruslan <ruslanpercovic391@gmail.com>
2026-02-25 13:45:45 +0200

    diploma

--- volunteer/volunteer/wsgi.py
+++ volunteer/volunteer/wsgi.py
@@ -1,7 +1,16 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer.settings')
 
 application = get_wsgi_application()
+
+call_command('migrate', '--noinput')
+call_command('collectstatic', '--noinput')
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin12345')
